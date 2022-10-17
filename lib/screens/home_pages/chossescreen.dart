229a920270d5655239b4256c3e7ad3a0717f8825
Arslan <maherarslan325@gmail.com>
2022-10-18 01:16:46 +0500

    choose sports from onboarding implemented

--- lib/screens/home_pages/chossescreen.dart
+++ lib/screens/home_pages/chossescreen.dart
@@ -1,6 +1,9 @@
 import 'package:azul_football/screens/favorites/favorites_clubs.dart';
+import 'package:azul_football/screens/home_pages/widgets/choose_sport_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import 'bottom_nav_screen.dart';
 
 class ChooseScree extends StatefulWidget {
   const ChooseScree({Key key}) : super(key: key);
@@ -13,56 +16,123 @@ class _ChooseScreeState extends State<ChooseScree> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final mSize = MediaQuery.of(context).size;
+    var size = MediaQuery.of(context).size;
+
+    /*24 is for notification bar on Android*/
+    final double itemHeight = (size.height - kToolbarHeight - 24) / 2;
+    final double itemWidth = size.width / 2;
     return Scaffold(
-      body: Center(
-          child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          GestureDetector(
-            onTap: () {
-              Get.to(
-                () => FavoritesClubsScreen(
-                  index: 0,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: size.height*.2,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("SPORTS",style: theme.textTheme.headline1,),
+                    Icon(Icons.arrow_forward,color: Colors.teal,)
+                  ],
                 ),
-                transition: Transition.fadeIn,
-              );
-            },
-            child: Container(
-              width: MediaQuery.of(context).size.width * .4,
-              height: MediaQuery.of(context).size.height * .33,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Image.asset(
-                  "assets/images/leagues/football.jpeg",
-                  fit: BoxFit.fill,
+                SizedBox(height: 20,),
+                GridView.count(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  crossAxisCount: 2,
+                  childAspectRatio: (1 / 1.3),
+                  padding: const EdgeInsets.all(0.0),
+                  mainAxisSpacing: 8.0,
+                  crossAxisSpacing: 8.0,
+                  children: [
+                    ChooseSportWidget(
+                      onTap: (){
+                        Get.to(
+                              () => BottomNavScreen(
+                            index: 1,
+                          ),
+                          transition: Transition.fadeIn,
+                        );
+                      },
+                      url: "assets/images/leagues/football.jpeg",
+                      title: "Football",
+                    ),
+                    ChooseSportWidget(
+                      onTap: (){
+                        Get.to(
+                              () => BottomNavScreen(
+                            index: 1,
+                          ),
+                          transition: Transition.fadeIn,
+                        );
+                      },
+                      url: "assets/images/leagues/basketball.jpeg",
+                      title: "Tennis",
+                    ),
+                    ChooseSportWidget(
+                      onTap: (){
+                        Get.to(
+                              () => BottomNavScreen(
+                            index: 1,
+                          ),
+                          transition: Transition.fadeIn,
+                        );
+                      },
+                      url: "assets/images/leagues/football.jpeg",
+                      title: "Cricket",
+                    ),
+                    ChooseSportWidget(
+                      onTap: (){
+                        Get.to(
+                              () => BottomNavScreen(
+                            index: 1,
+                          ),
+                          transition: Transition.fadeIn,
+                        );
+                      },
+                      url: "assets/images/leagues/football.jpeg",
+                      title: "Football",
+                    ),
+                    ChooseSportWidget(
+                      onTap: (){
+                        Get.to(
+                              () => BottomNavScreen(
+                            index: 1,
+                          ),
+                          transition: Transition.fadeIn,
+                        );
+                      },
+                      url: "assets/images/leagues/football.jpeg",
+                      title: "Football",
+                    ),
+                    ChooseSportWidget(
+                      onTap: (){
+                        // Get.to(
+                        //       () => FavoritesClubsScreen(
+                        //     index: 1,
+                        //   ),
+                        //   transition: Transition.fadeIn,
+                        // );
+                        Get.to(
+                              () => BottomNavScreen(
+                            index: 1,
+                          ),
+                          transition: Transition.fadeIn,
+                        );
+                      },
+                      url: "assets/images/leagues/basketball.jpeg",
+                      title: "Tennis",
+                    ),
+                  ],
                 ),
-              ),
+              ],
             ),
           ),
-          GestureDetector(
-            onTap: () {
-              Get.to(
-                () => FavoritesClubsScreen(
-                  index: 1,
-                ),
-                transition: Transition.fadeIn,
-              );
-            },
-            child: Container(
-              width: MediaQuery.of(context).size.width * .4,
-              height: MediaQuery.of(context).size.height * .33,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Image.asset(
-                  "assets/images/leagues/basketball.jpeg",
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          ),
-        ],
-      )),
+        ),
+      ),
     );
   }
 }
